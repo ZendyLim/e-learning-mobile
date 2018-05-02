6b5ca96502e1dcbe8fe9699a1ef13df06a674b43
davidpborromeo <davidpborromeo@gmail.com>
2018-05-02 22:57:38 +0900

    Fix: Initial setup

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to JLPT5 E-learning 
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
