e355acf432134c5445a08f5ad46397f9965b2c5f
huy <huyvt2014@gmail.com>
2018-05-10 20:27:46 +0900

    setting page UI

--- app/components/Setting/setting.js
+++ app/components/Setting/setting.js
@@ -3,10 +3,13 @@ import {
     ActivityIndicator,
     AsyncStorage,
     Button,
+    TouchableHighlight,
     StatusBar,
     StyleSheet,
     Text,
     View,
+    Image,
+    ScrollView,
   } from 'react-native';
   import { List, ListItem } from 'react-native-elements';
   
@@ -15,7 +18,7 @@ import {
   import * as Actions from '../../actions/user'; //Import your actions
   
   class SettingScreen extends Component {
-  
+  d
     static navigationOptions = {
       header: null,
       title: 'Study',
@@ -24,12 +27,67 @@ import {
     render() {
       console.log(this.props);
       return (
-        <View style={styles.container}>
-        <Text>ID : { this.props.data.id } </Text>
-        <Text>Name : { this.props.data.name } </Text>
-        
-          <Button title="Setting Page" onPress={this.selectList} />
-        </View>
+        // <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <View style={styles.topbackground}>
+           </View>
+
+          <View style={styles.downbackground}>
+            </View>
+
+          <View style={styles.mainscreen} >
+            <View style={styles.topinfor}>
+              <View style={styles.avatarinformation}>
+                <Text style={styles.txtbiginfor}>
+                  kanako16
+                  {/* ID: { this.props.data.id } */}
+                 </Text>
+                <Text>
+                  {/* Name: { this.props.data.name } */}
+                  kanako16@gmail.com
+                 </Text>
+               </View>
+              <Image 
+                style={styles.avatar} 
+                source={{uri: 'https://www.iconspng.com/images/funny-monkey-face/funny-monkey-face.jpg'}} />
+             </View>
+            <View style={styles.downinfor}>
+              <Text style={styles.txtinfor}>School</Text>
+              <Text style={styles.txtdata}>BITTTTTTTTTTTTTTTTTTTTTTTTTTTTTT</Text>
+              <View style={styles.vline}></View>
+              <Text style={styles.txtinfor}>Nationaly</Text>
+              <Text style={styles.txtdata}>Chinese</Text>
+              <View style={styles.vline}></View>
+              <Text style={styles.txtbiginfor}>Study Hours</Text>
+              <Text style={styles.txtinfor}>How many times per week?</Text>
+              <Text style={styles.txtdata}>6</Text>
+              <View style={styles.vline}></View>
+              <Text style={styles.txtinfor}>How many times per day</Text>
+              <Text style={styles.txtdata}>6</Text>
+              <View style={styles.vline}></View>
+              <TouchableHighlight style={styles.btnupdate} onPress={this.updateProfile}>
+                <Text style={styles.txtbutton}>Update</Text>
+              </TouchableHighlight>
+              <Text style={styles.txtbiginfor}>Account</Text>
+              <Text style={styles.txtinfor}>Sign in:</Text>
+              <View style={styles.vgsignin}>
+                <TouchableHighlight style={styles.btnsigning} onPress={this.selectList}>
+                  <Text style={styles.txtbutton}>
+                    <Text style={styles.txtbuttonicon}>G </Text>
+                    Google
+                  </Text>
+                </TouchableHighlight>
+                <TouchableHighlight style={styles.btnsigninf} onPress={this.selectList}> 
+                  <Text style={styles.txtbutton}>
+                    <Text style={styles.txtbuttonicon}>f </Text>
+                    Facebook
+                  </Text>
+                </TouchableHighlight>
+              </View>
+             </View>
+           </View>
+          </ScrollView>
+        //  </View>
       );
     }
   
@@ -38,6 +96,14 @@ import {
       //await AsyncStorage.setItem('userToken', 'abc');
       this.props.navigation.navigate('HiraganaList');
     };
+    updateProfile = () => {
+      var updateValue = {
+        // user : "facebook",
+        id: "2",
+        name: "test",
+      }
+      this.props.updateProfile(updateValue);
+    }
   }
 
   const styles = require('../../styles/style');
--- app/styles/style.js
+++ app/styles/style.js
@@ -8,10 +8,139 @@ var {
 
 module.exports = StyleSheet.create({
 
-    container: {
+    // container: {
+    //     flex: 1,
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //     position: 'relative',
+    //     flexDirection: 'row',
+    //   },
+
+       container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+		position: 'relative',
+		// height: '100%',
+		// width: '100%',
       },
 
+    topbackground: {
+    	backgroundColor: '#d2f9fc',
+    	flex: 26,
+    	width: '100%',
+ 	 },
+
+ 	downbackground: {
+    	backgroundColor: 'white',
+    	flex: 74,
+    	width: '100%',
+	 },
+	  
+	mainscreen: {
+		 position: 'absolute',
+		 width: '100%',
+		 height: '100%',
+		 alignItems: 'center', 
+		   
+	},
+	
+	topinfor: {
+		width: '82%',
+		alignItems: 'center',
+		marginTop: 20,
+	},
+
+   downinfor: {
+		width: '92%',
+		alignContent: 'space-between',
+	  marginTop: 5, 
+	},
+
+    avatar: {
+       borderWidth:1,
+       borderColor:'black',
+       width:120,
+       height:120,
+	   position: 'absolute',
+       borderRadius:100,
+   	 },
+
+ 	avatarinformation: {
+ 	 	width: '100%',
+ 	 	height: 150,
+ 	 	backgroundColor: 'white',
+ 	 	marginTop: 60,
+ 	 	alignItems: 'center',
+ 	 	justifyContent: 'flex-start',
+ 	 	paddingTop: 70,
+ 	 	borderRadius: 5,
+ 	 	borderWidth: 1,
+ 	 	borderColor: 'black',
+	  },
+	  
+ 	txtinfor: {
+ 		fontWeight: 'bold',
+ 		fontSize: 12,
+		marginStart: 5,
+		marginTop: 5,
+ 		color: 'black',
+ 	},
+ 	txtdata: {
+ 		fontSize: 15,
+ 		marginStart: 5, 
+ 		color: 'grey', 
+		marginTop: 5,
+ 	},
+ 	vline: {
+ 		borderWidth: 0.7, 
+ 		borderColor:'#cccccc',
+		marginTop: 5,
+		// marginBottom: 5,
+ 	},
+ 	txtbiginfor: {
+ 		fontSize: 15,
+		marginStart: 5,
+		marginTop: 5,
+ 		color: '#9ba1ca',
+	 },
+	vgsignin: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginTop: 5,
+	},
+	btnupdate: {
+		backgroundColor: '#4573e7',
+		borderRadius: 5,
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '100%',
+		height: 40,
+		marginTop: 5,
+	},
+	btnsigning: {
+		backgroundColor: '#ec6f86',
+		borderRadius: 5,
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexDirection: 'row',
+		width: '48%',
+		height: 40,
+	},
+	btnsigninf: {
+		backgroundColor: '#4573e7',
+		borderRadius: 5,
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexDirection: 'row',
+		width: '48%',
+		height: 40,
+	},
+	txtbutton: {
+		fontFamily: 'Roboto',
+		color: 'white',
+		fontWeight: 'bold',
+	},
+	txtbuttonicon: {
+		fontSize: 16,
+	},
 });
