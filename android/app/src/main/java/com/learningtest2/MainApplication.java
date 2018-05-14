ef4861e284d351e0e56b401c63841927baf874a3
davidpborromeo <davidpborromeo@gmail.com>
2018-05-14 20:02:01 +0900

    #20 Remove: duplicate google sign in

--- android/app/src/main/java/com/learningtest2/MainApplication.java
+++ android/app/src/main/java/com/learningtest2/MainApplication.java
@@ -28,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNGoogleSigninPackage(),
+          new VectorIconsPackage(),
           new RNGoogleSigninPackage(),
           new ReactNativeLocalizationPackage(),
           new FacebookLoginPackage()
