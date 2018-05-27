89432c8ac84798ef297f0caaa6647a00c3c45f44
Unknown <andy.william@bit-okutama.jp>
2018-05-27 23:08:01 +0900

    Summary Fix Data
    Summary Fix Data

--- app/screens/Summary/studySummary.js
+++ app/screens/Summary/studySummary.js
@@ -6,9 +6,14 @@ import {
     StatusBar,
     StyleSheet,
     View,
+    Text,
+    TouchableHighlight,
+    TouchableOpacity,
+    Image,
   } from 'react-native';
-  import { List, ListItem } from 'react-native-elements';
+  import { List, ListItem , Icon} from 'react-native-elements';
   import * as Actions from '../../actions/user'; //Import your actions
+  import { ProgressCircle , LineChart, Grid, YAxis, XAxis }  from 'react-native-svg-charts'
 
   import { bindActionCreators } from 'redux';
   import { connect } from 'react-redux';
@@ -19,26 +24,121 @@ import {
       header: null,
       title: 'Summary',
     };
-    _showMoreApp = async () => {
-      this.props.deleteUserState();
-      this.props.navigation.navigate('AuthLoading');
-    };  
+
+
+    gotoSelectTopic = (typeValue) => {
+      //await AsyncStorage.setItem('userToken', 'abc');
+      this.props.navigation.navigate('PickTopicSummary', {
+        type : typeValue
+      });
+    };
+    
     render() {
+      const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+      const contentInsetY = { top: 20, bottom: 20 }
+      const contentInsetX = { top: 20, bottom: 20 }
       return (
-        <View style={styles.containerCenter}>
-         <Button title="LogOut" onPress={this._showMoreApp} />          
+        <View style={scoreStyle.scoreContainer}>
+          <View style={summary.summaryMainTop}>
+            <View style={summary.flexTop1}>
+              <View style={ summary.absoluteTextHome }>
+                <View style={ summary.containerScoreHome }>
+                  <Text style={ summary.persentTitle }>100%</Text>
+                </View>
+              </View>
+              <ProgressCircle
+                  style={ { height: 150 } }
+                  progress={ (70/100) }
+                  progressColor={'#43b5e7'}
+              />
+            </View>         
+            <View style={summary.flexTop2}>
+              <Text style={ summary.toptitle }>Days Left</Text>
+              <Text style={ summary.topsub }>40 days</Text>
+              <Text style={ summary.toptitle }>Target Date</Text>
+              <Text style={ summary.topsub }>September 18, 2018</Text>
+            </View>         
+          </View>     
+          <View style={summary.summaryMainTop}>
+            <View style={summary.flexMid1}>
+              <View style={summary.midContainer}>
+                <LineChart
+                  style={{ height: 200 }}
+                  data={ data }
+                  svg={{ stroke: 'rgb(134, 65, 244)' }}
+                  contentInset={{ top: 20, bottom: 20 }}
+                >
+                <Grid/>
+                </LineChart>
+              </View>
+            </View>
+          </View>    
+          <View style={summary.summaryMainTop}>
+            <View style={summary.flexRow}>
+              <View style={summary.flexBot1}>
+                <View   style={summary.conGraph}>
+                  <TouchableHighlight  onPress={this.gotoSelectTopic.bind(this, 'vocabulary')}>
+                    <View style={summary.graphButton}>
+                    <View style={ summary.absoluteGr }>
+                        <Text style={ summary.btnText }>200/200{"\n"}Words{"\n"}learned</Text>
+                      </View>
+                      <Text style={ summary.TextGr }>Vocabulary</Text>
+                      <ProgressCircle
+                        style={ { height: 80 } }
+                        progress={ (70/100) }
+                        progressColor={'#b3ee68'}
+                      />  
+                    </View>
+                  </TouchableHighlight >                  
+                </View>
+              </View>
+              <View style={summary.flexBot1}>
+                <View   style={summary.conGraph}>
+                <TouchableHighlight  onPress={this.gotoSelectTopic.bind(this, 'grammar')}>
+                  <View style={summary.graphButton}>
+                    <View style={ summary.absoluteGr }>
+                        <Text style={ summary.btnText }>200/200{"\n"}Words{"\n"}learned</Text>
+                      </View>
+                      <Text style={ summary.TextGr }>Grammar</Text>
+                      <ProgressCircle
+                        style={ { height: 80 } }
+                        progress={ (70/100) }
+                        progressColor={'#43b5e7'}
+                      />  
+                    </View>
+                  </TouchableHighlight >                  
+                </View>
+              </View>
+              <View style={summary.flexBot1}>
+                <View   style={summary.conGraph}>
+                <TouchableHighlight  onPress={this.gotoSelectTopic.bind(this, 'kanji')}>
+                    <View style={summary.graphButton}>
+                    <View style={ summary.absoluteGr }>
+                        <Text style={ summary.btnText }>200/200{"\n"}Words{"\n"}learned</Text>
+                      </View>
+                      <Text style={ summary.TextGr }>Kanji</Text>
+                      <ProgressCircle
+                        style={ { height: 80 } }
+                        progress={ (70/100) }
+                        progressColor={'#ea7085'}
+                      />  
+                    </View>
+                  </TouchableHighlight >                  
+                </View>
+              </View>              
+            </View>
+          </View>    
         </View>
       );
     }
   
     //createGuest = async () => {
-    createGuest = () => {
-      //await AsyncStorage.setItem('userToken', 'abc');
-      this.props.navigation.navigate('NameIn');
-    };
+  
   }
 
   const styles = require('../../styles/style');
+  const summary = require('../../styles/summary');
+  const scoreStyle = require('../../styles/score');
   // The function takes data from the app current state,
 // and insert/links it into the props of our component.
 // This function makes Redux know that this component needs to be passed a piece of the state
---
+++ app/screens/Summary/summaryLearn.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import {
+    ActivityIndicator,
+    AsyncStorage,
+    Button,
+    StatusBar,
+    StyleSheet,
+    View,
+    ScrollView,
+    Text,
+    Image,
+    TouchableOpacity, 
+} from 'react-native';
+import { Icon } from 'react-native-elements';
+import  { strings }   from '../../config/localization';
+import { LearnListData } from '../../config/studyList';
+import style from 'react-native-datepicker/style';
+import { summaryStyle } from '../../config/summaryLearn';
+import { ImageData } from '../../config/image_list';
+
+class SummaryLearnDetail extends Component {
+  constructor() {
+    super();
+    this._onSetLanguageTo('en');
+  }
+  _onSetLanguageTo(value) {
+    strings.setLanguage(value);
+  } 
+  static navigationOptions = {
+    title: 'Learn',
+  };
+  state = {
+    type: "",
+  }
+  componentDidMount() {
+    const { navigation } = this.props;
+    this.setState({
+        studyType: navigation.getParam('type', null),
+    });
+  }
+ 
+  render() {
+    if(this.state.studyType ){
+      return (
+        <ScrollView style={study.StudyContainer}>
+            <Image 
+              style={ study.cardImg }
+              source= { ImageData['loading'] }
+              resizeMode="stretch"  
+            />
+                <View style={study.summaryLearn} >
+                    <Text style={[study.summaryTitle, study.fontBold]}>TITLE</Text>
+                    <Text style={[study.summaryDetail, study.fontBold]}>SCORE</Text>
+                </View>
+            { summaryStyle.map((item, key)=>(
+                <View key={key} style={study.summaryLearn} >
+                    <Text style={study.summaryTitle}>{ item.title }</Text>
+                    <Text style={study.summaryDetail}>{ item.totalCorrect }/{ item.totalLearn }</Text>
+                </View>
+            ))}
+        </ScrollView>
+      );
+    }else{
+      return (
+        <Text>none</Text> 
+      );
+    }
+  }
+
+
+}
+
+const styles = require('../../styles/style');
+const study = require('../../styles/study');
+
+export default SummaryLearnDetail;
